aa9f33dd72161e040932b9123c51ad9a2eb1784a
geeatwork777 <claas_baller@yahoo.com>
2022-05-19 18:08:32 +0800

    Added fallback mapping for potential removal of tokenURI mapping in supercontract in the future. Set BaseURI to new cloud location with override of according function. Fixed metadata interface URI function to point to full URI (with base)

--- Main/contracts/SmartIdentity.sol
+++ Main/contracts/SmartIdentity.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol"; //potentially remove this, because we do not want to allow transfers without approval
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 
@@ -12,27 +12,34 @@ contract SmartIdentity is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 	address[] devs;
-    mapping(uint => string) public CIDMapping;
+    mapping(uint => string) public IdToUri;
 
 
     constructor() ERC721("SmartIdentity", "ID") {
 	    devs[0] = 0x3Bb4404CCCf81156b692874AfFf7083CD7d73d32;
 	}
 
-    function mintNFT(address recipient, string memory tokenURI)
+    function mintNFT(address recipient, string memory URI)
         public onlyOwner
         returns (uint256)
     {
         _tokenIds.increment();
-
         uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-        string memory CIDPlaceholder = "PlaceholderforCID";
-        CIDMapping[newItemId] = CIDPlaceholder;
+        _setTokenURI(newItemId, URI);
+		_setTokenURIFallBack(newItemId, URI);
 
         return newItemId;
     }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return "https://res.cloudinary.com/dv14w4qd6/raw/upload/v1652951579/";
+    }
+	
+	function _setTokenURIFallBack(uint256 tokenId, string memory URI) internal virtual{
+	    require(_exists(tokenId), "ERC721URIStorage: URI set of nonexistent token");
+		IdToUri[tokenId] = URI;
+	}
 
     function checkDevs() internal returns(bool success) {
     for (uint i = 0; i < devs.length; i++) {
@@ -84,7 +91,7 @@ contract SmartIdentity is ERC721URIStorage, Ownable {
      */
     function tokenURI(uint256 tokenId) public view override returns (string memory){
         //Should include the pinata link https://gateway.pinata.cloud/ipfs/<metadata-hash-code>
-        string memory cid = CIDMapping[tokenId];
-        return cid;
+        string memory URI = IdToUri[tokenId];
+        return string(abi.encodePacked(_baseURI(), URI));
     }
 }
