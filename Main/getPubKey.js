e8816b06186b90c39977ee1101dceeb6aa44a715
geeatwork777 <51171693+geeatwork777@users.noreply.github.com>
2022-05-25 19:57:24 +0800

    Update getPubKey.js

--- Main/getPubKey.js
+++ Main/getPubKey.js
@@ -1,6 +1,6 @@
 var Wallet = require('ethereumjs-wallet');
 var EthUtil = require('ethereumjs-util');
-const privateKeyString = '0x9fdbe4e81e07e1f314b3af6b24f953ae60b6ec732f3f425ebe25463b3e544c08';
+const privateKeyString = <Your-private-Key>;
 const privateKeyBuffer = EthUtil.toBuffer(privateKeyString);
 const wallet = Wallet['default'].fromPrivateKey(privateKeyBuffer);
 const publicKey = wallet.getPublicKeyString();
